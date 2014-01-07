0ffb17c6e08a77c314a1c6daafa134ba3248bc41
Dave Ackley <ackley@ackleyshack.com>
2014-01-07 14:41:56 -0700

    Add a 'fail' mechanism, with a FAIL() macro, to be something like an exception or abort, for general runtime use.  Usage e.g.,: FAIL(ILLEGAL_ARGUMENT);.  Add more fail codes as needed in src/include/failCodes.h

---
+++ src/fail.cpp
@@ -0,0 +1,24 @@
+extern "C" {
+#include <stdlib.h>  /* for abort() */
+#include <stdio.h>  /* for fprintf, stderr */
+#include "fail.h"
+
+  static const char * FailStrings[] = {
+    "zero unused",
+#define XX(x) #x, 
+#include "failCodes.h"
+#undef XX
+    "max failure code"
+  };
+
+  const char * FailCodeReason(int failCode) {
+    if (failCode < 0 || failCode >= (int) (sizeof(FailStrings)/sizeof(FailStrings[0])))
+      return "[failCode out of range]";
+    return FailStrings[failCode];
+  }
+
+  void FailHere(const char * fname, const int lineno, int code) {
+    fprintf(stderr,"%s:%d: FAILED: %s\n", fname, lineno, FailCodeReason(code));
+    abort();
+  }
+}
---
+++ src/failCodes.cpp
@@ -0,0 +1,2 @@
+/* Note: failCodes.h is included by fail.h and fail.cpp.  This file
+   should always remain empty except for this message */
