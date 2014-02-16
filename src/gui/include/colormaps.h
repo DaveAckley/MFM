c026eb6603f85626ff43da17726d7470cac34391
Dave Ackley <ackley@ackleyshack.com>
2014-02-16 09:48:16 -0700

    Add support for a range of color scales and gradients; with some minimal tests

---
+++ src/gui/include/colormaps.h
@@ -0,0 +1,115 @@
+/**
+ * This file includes color specifications and designs developed by
+ * Cynthia Brewer (http://colorbrewer.org/)
+ *
+ * Color data from colorbrewer2.org, adapted to MFMv2 by Ackley 2014.
+ *
+ * ColorBrewer Color Schemes are licensed under the following license
+ * (which -- please note -- has no clause 3):
+ ********************************************************************
+ * Apache-Style Software License for ColorBrewer software and
+ * ColorBrewer Color Schemes
+ *
+ * Copyright (c) 2002 Cynthia Brewer, Mark Harrower, and The
+ * Pennsylvania State University.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.  You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions as source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowledgment: This product
+ *    includes color specifications and designs developed by Cynthia
+ *    Brewer (http://colorbrewer.org/).  Alternately, this
+ *    acknowledgment may appear in the software itself, if and
+ *    wherever such third-party acknowledgments normally appear.
+ * 4. The name "ColorBrewer" must not be used to endorse or promote
+ *    products derived from this software without prior written
+ *    permission.  For written permission, please contact Cynthia
+ *    Brewer at cbrewer@psu.edu.
+ * 5. Products derived from this software may not be called
+ *    "ColorBrewer", nor may "ColorBrewer" appear in their name,
+ *    without prior written permission of Cynthia Brewer.
+ */
+
+/* Sequential scales */
+XX5(SEQ, BuGn, 0xffedf8fb, 0xffb2e2e2, 0xff66c2a4, 0xff2ca25f, 0xff006d2c)
+XX6(SEQ, BuGn, 0xffedf8fb, 0xffccece6, 0xff99d8c9, 0xff66c2a4, 0xff2ca25f, 0xff006d2c)
+XX5(SEQ, BuPu, 0xffedf8fb, 0xffb3cde3, 0xff8c96c6, 0xff8856a7, 0xff810f7c)
+XX6(SEQ, BuPu, 0xffedf8fb, 0xffbfd3e6, 0xff9ebcda, 0xff8c96c6, 0xff8856a7, 0xff810f7c)
+XX5(SEQ, GnBu, 0xfff0f9e8, 0xffbae4bc, 0xff7bccc4, 0xff43a2ca, 0xff0868ac)
+XX6(SEQ, GnBu, 0xfff0f9e8, 0xffccebc5, 0xffa8ddb5, 0xff7bccc4, 0xff43a2ca, 0xff0868ac)
+XX5(SEQ, PuBu, 0xfff1eef6, 0xffbdc9e1, 0xff74a9cf, 0xff2b8cbe, 0xff045a8d)
+XX6(SEQ, PuBu, 0xfff1eef6, 0xffd0d1e6, 0xffa6bddb, 0xff74a9cf, 0xff2b8cbe, 0xff045a8d)
+XX5(SEQ, PuBuGn, 0xfff6eff7, 0xffbdc9e1, 0xff67a9cf, 0xff1c9099, 0xff016c59)
+XX6(SEQ, PuBuGn, 0xfff6eff7, 0xffd0d1e6, 0xffa6bddb, 0xff67a9cf, 0xff1c9099, 0xff016c59)
+XX5(SEQ, PuRd, 0xfff1eef6, 0xffd7b5d8, 0xffdf65b0, 0xffdd1c77, 0xff980043)
+XX6(SEQ, PuRd, 0xfff1eef6, 0xffd4b9da, 0xffc994c7, 0xffdf65b0, 0xffdd1c77, 0xff980043)
+XX5(SEQ, RdPu, 0xfffeebe2, 0xfffbb4b9, 0xfff768a1, 0xffc51b8a, 0xff7a0177)
+XX6(SEQ, RdPu, 0xfffeebe2, 0xfffcc5c0, 0xfffa9fb5, 0xfff768a1, 0xffc51b8a, 0xff7a0177)
+XX5(SEQ, YlGn, 0xffffffcc, 0xffc2e699, 0xff78c679, 0xff31a354, 0xff006837)
+XX6(SEQ, YlGn, 0xffffffcc, 0xffd9f0a3, 0xffaddd8e, 0xff78c679, 0xff31a354, 0xff006837)
+XX5(SEQ, YlGnBu, 0xffffffcc, 0xffa1dab4, 0xff41b6c4, 0xff2c7fb8, 0xff253494)
+XX6(SEQ, YlGnBu, 0xffffffcc, 0xffc7e9b4, 0xff7fcdbb, 0xff41b6c4, 0xff2c7fb8, 0xff253494)
+XX5(SEQ, YlOrBr, 0xffffffd4, 0xfffed98e, 0xfffe9929, 0xffd95f0e, 0xff993404)
+XX6(SEQ, YlOrBr, 0xffffffd4, 0xfffee391, 0xfffec44f, 0xfffe9929, 0xffd95f0e, 0xff993404)
+XX5(SEQ, YlOrRd, 0xffffffb2, 0xfffecc5c, 0xfffd8d3c, 0xfff03b20, 0xffbd0026)
+XX6(SEQ, YlOrRd, 0xffffffb2, 0xfffed976, 0xfffeb24c, 0xfffd8d3c, 0xfff03b20, 0xffbd0026)
+XX5(SEQ, Greys, 0xfff7f7f7, 0xffcccccc, 0xff969696, 0xff636363, 0xff252525)
+XX6(SEQ, Greys, 0xfff7f7f7, 0xffd9d9d9, 0xffbdbdbd, 0xff969696, 0xff636363, 0xff252525)
+XX5(SEQ, Reds, 0xfffee5d9, 0xfffcae91, 0xfffb6a4a, 0xffde2d26, 0xffa50f15)
+XX6(SEQ, Reds, 0xfffee5d9, 0xfffcbba1, 0xfffc9272, 0xfffb6a4a, 0xffde2d26, 0xffa50f15)
+
+/* Diverging scales */
+XX5(DIV, BrBG, 0xffa6611a, 0xffdfc27d, 0xfff5f5f5, 0xff80cdc1, 0xff018571)
+XX6(DIV, BrBG, 0xff8c510a, 0xffd8b365, 0xfff6e8c3, 0xffc7eae5, 0xff5ab4ac, 0xff01665e)
+XX5(DIV, PiYG, 0xffd01c8b, 0xfff1b6da, 0xfff7f7f7, 0xffb8e186, 0xff4dac26)
+XX6(DIV, PiYG, 0xffc51b7d, 0xffe9a3c9, 0xfffde0ef, 0xffe6f5d0, 0xffa1d76a, 0xff4d9221)
+XX5(DIV, PRGn, 0xff7b3294, 0xffc2a5cf, 0xfff7f7f7, 0xffa6dba0, 0xff008837)
+XX6(DIV, PRGn, 0xff762a83, 0xffaf8dc3, 0xffe7d4e8, 0xffd9f0d3, 0xff7fbf7b, 0xff1b7837)
+XX5(DIV, PuOr, 0xffe66101, 0xfffdb863, 0xfff7f7f7, 0xffb2abd2, 0xff5e3c99)
+XX6(DIV, PuOr, 0xffb35806, 0xfff1a340, 0xfffee0b6, 0xffd8daeb, 0xff998ec3, 0xff542788)
+XX5(DIV, RdBu, 0xffca0020, 0xfff4a582, 0xfff7f7f7, 0xff92c5de, 0xff0571b0)
+XX6(DIV, RdBu, 0xffb2182b, 0xffef8a62, 0xfffddbc7, 0xffd1e5f0, 0xff67a9cf, 0xff2166ac)
+XX5(DIV, RdGy, 0xffca0020, 0xfff4a582, 0xffffffff, 0xffbababa, 0xff404040)
+XX6(DIV, RdGy, 0xffb2182b, 0xffef8a62, 0xfffddbc7, 0xffe0e0e0, 0xff999999, 0xff4d4d4d)
+XX5(DIV, RdYlBu, 0xffd7191c, 0xfffdae61, 0xffffffbf, 0xffabd9e9, 0xff2c7bb6)
+XX6(DIV, RdYlBu, 0xffd73027, 0xfffc8d59, 0xfffee090, 0xffe0f3f8, 0xff91bfdb, 0xff4575b4)
+XX5(DIV, RdYlGn, 0xffd7191c, 0xfffdae61, 0xffffffbf, 0xffa6d96a, 0xff1a9641)
+XX6(DIV, RdYlGn, 0xffd73027, 0xfffc8d59, 0xfffee08b, 0xffd9ef8b, 0xff91cf60, 0xff1a9850)
+XX5(DIV, Spectral, 0xffd7191c, 0xfffdae61, 0xffffffbf, 0xffabdda4, 0xff2b83ba)
+XX6(DIV, Spectral, 0xffd53e4f, 0xfffc8d59, 0xfffee08b, 0xffe6f598, 0xff99d594, 0xff3288bd)
+
+/* Qualititative scales */
+XX5(QUAL, Dark2, 0xff1b9e77, 0xffd95f02, 0xff7570b3, 0xffe7298a, 0xff66a61e)
+XX6(QUAL, Dark2, 0xff1b9e77, 0xffd95f02, 0xff7570b3, 0xffe7298a, 0xff66a61e, 0xffe6ab02)
+XX5(QUAL, Paired, 0xffa6cee3, 0xff1f78b4, 0xffb2df8a, 0xff33a02c, 0xfffb9a99)
+XX6(QUAL, Paired, 0xffa6cee3, 0xff1f78b4, 0xffb2df8a, 0xff33a02c, 0xfffb9a99, 0xffe31a1c)
+XX5(QUAL, Set1, 0xffe41a1c, 0xff377eb8, 0xff4daf4a, 0xff984ea3, 0xffff7f00)
+XX6(QUAL, Set1, 0xffe41a1c, 0xff377eb8, 0xff4daf4a, 0xff984ea3, 0xffff7f00, 0xffffff33)
+XX5(QUAL, Set2, 0xff66c2a5, 0xfffc8d62, 0xff8da0cb, 0xffe78ac3, 0xffa6d854)
+XX6(QUAL, Set2, 0xff66c2a5, 0xfffc8d62, 0xff8da0cb, 0xffe78ac3, 0xffa6d854, 0xffffd92f)
+
+/* Colormap debugging scales */
+XX5(DBG, BKWH, 0xff000000, 0xff3f3f3f, 0xff7f7f7f, 0xffbfbfbf, 0xffffffff)
+XX6(DBG, BKWH, 0xff000000, 0xff333333, 0xff666666, 0xff999999, 0xffcccccc, 0xffffffff)
+XX5(DBG, WHBK, 0xffffffff, 0xffbfbfbf, 0xff7f7f7f, 0xff3f3f3f, 0xff000000)
+XX6(DBG, WHBK, 0xffffffff, 0xffcccccc, 0xff999999, 0xff666666, 0xff333333, 0xff000000)
+XX5(DBG, RDBU, 0xffff0000, 0xffbf003f, 0xff7f007f, 0xff3f00bf, 0xff0000ff)
+XX6(DBG, RDBU, 0xffff0000, 0xffcc0033, 0xff990066, 0xff660099, 0xff3300cc, 0xff0000ff)
+
---
+++ src/test/src/colormap_test.cpp
@@ -0,0 +1,88 @@
+#include "assert.h"
+#include "colormap_test.h"
+#include "itype.h"
+
+namespace MFM {
+
+  static void assertColorsClose(u32 c1, u32 c2) {
+    for (u32 i = 0; i < 4; ++i) {
+      s32 comp1 = (c1 >> (i << 3)) & 0xff;
+      s32 comp2 = (c2 >> (i << 3)) & 0xff;
+
+      assert(comp1 - comp2 < 2 && comp2 - comp1 < 2);
+    }
+  }
+
+  void ColorMapTest::Test_RunTests() {
+    Test_colorMapSelected();
+    Test_colorMapInterpolated();
+  }
+
+  void ColorMapTest::Test_colorMapSelected()
+  {
+    for (u32 i = 0; i < 5; ++i) {
+      u32 frac = i*255/4;  // five even steps from black to white..
+      u32 color = 0xff000000|(frac<<16)|(frac<<8)|(frac);
+      assert(ColorMap_DBG5_BKWH::THE_INSTANCE.GetSelectedColor(i,0,4,0xffff0000)==color);
+
+      assert(ColorMap_DBG5_BKWH::THE_INSTANCE.GetSelectedColor(i*10,0,4*10,0xffff0000)==color);
+      if (i!=0) {
+        assert(ColorMap_DBG5_BKWH::THE_INSTANCE.GetSelectedColor(i*10-1,0,4*10,0xffff0000)==color);
+        assert(ColorMap_DBG5_BKWH::THE_INSTANCE.GetSelectedColor(i*10-2,0,4*10,0xffff0000)==color);
+      }
+      if (i!=4) {
+        assert(ColorMap_DBG5_BKWH::THE_INSTANCE.GetSelectedColor(i*10+1,0,4*10,0xffff0000)==color);
+        assert(ColorMap_DBG5_BKWH::THE_INSTANCE.GetSelectedColor(i*10+2,0,4*10,0xffff0000)==color);
+      }
+    }
+    
+  }
+
+  void ColorMapTest::Test_colorMapInterpolated()
+  {
+    // Half scale should be 50% grey on 5 or 6 scales, forward or backwards
+    assertColorsClose(ColorMap_DBG5_BKWH::THE_INSTANCE.GetInterpolatedColor(0.5,0,1,0xffff0000),0xff808080);
+    assertColorsClose(ColorMap_DBG6_BKWH::THE_INSTANCE.GetInterpolatedColor(0.5,0,1,0xffff0000),0xff808080);
+    assertColorsClose(ColorMap_DBG5_WHBK::THE_INSTANCE.GetInterpolatedColor(0.5,0,1,0xffff0000),0xff808080);
+    assertColorsClose(ColorMap_DBG6_WHBK::THE_INSTANCE.GetInterpolatedColor(0.5,0,1,0xffff0000),0xff808080);
+
+    // 0.0 should be black on either scale, and rev for rev scale
+    assertColorsClose(ColorMap_DBG5_BKWH::THE_INSTANCE.GetInterpolatedColor(0.0,0,1,0xffff0000),0xff000000);
+    assertColorsClose(ColorMap_DBG6_BKWH::THE_INSTANCE.GetInterpolatedColor(0.0,0,1,0xffff0000),0xff000000);
+    assertColorsClose(ColorMap_DBG5_WHBK::THE_INSTANCE.GetInterpolatedColor(1.0,0,1,0xffff0000),0xff000000);
+    assertColorsClose(ColorMap_DBG6_WHBK::THE_INSTANCE.GetInterpolatedColor(1.0,0,1,0xffff0000),0xff000000);
+
+    // 1.0 should be white on either scale, and rev for rev scale
+    assertColorsClose(ColorMap_DBG5_BKWH::THE_INSTANCE.GetInterpolatedColor(1.0,0,1,0xffff0000),0xffffffff);
+    assertColorsClose(ColorMap_DBG6_BKWH::THE_INSTANCE.GetInterpolatedColor(1.0,0,1,0xffff0000),0xffffffff);
+    assertColorsClose(ColorMap_DBG5_WHBK::THE_INSTANCE.GetInterpolatedColor(0.0,0,1,0xffff0000),0xffffffff);
+    assertColorsClose(ColorMap_DBG6_WHBK::THE_INSTANCE.GetInterpolatedColor(0.0,0,1,0xffff0000),0xffffffff);
+
+    // Out of range values should be caught
+    assert(ColorMap_DBG5_BKWH::THE_INSTANCE.GetInterpolatedColor(1.00001,0,1,0xffff0000)==0xffff0000);
+    assert(ColorMap_DBG6_BKWH::THE_INSTANCE.GetInterpolatedColor(-0.0001,0,1,0xffff0000)==0xffff0000);
+
+    // Other equivalent scales should be okay
+    assertColorsClose(ColorMap_DBG6_BKWH::THE_INSTANCE.GetInterpolatedColor(5,0,10,0xffff0000),0xff808080);
+    assertColorsClose(ColorMap_DBG5_WHBK::THE_INSTANCE.GetInterpolatedColor(-100,-350,150,0xffff0000),0xff808080);
+    assertColorsClose(ColorMap_DBG5_BKWH::THE_INSTANCE.GetInterpolatedColor(1,1,3,0xffff0000),0xff000000);
+    assertColorsClose(ColorMap_DBG6_BKWH::THE_INSTANCE.GetInterpolatedColor(0.0,0,1000,0xffff0000),0xff000000);
+    assertColorsClose(ColorMap_DBG5_BKWH::THE_INSTANCE.GetInterpolatedColor(-2.0,-12,-2,0xffff0000),0xffffffff);
+    assertColorsClose(ColorMap_DBG5_WHBK::THE_INSTANCE.GetInterpolatedColor(9,8.9,9,0xffff0000),0xff000000);
+
+    assert(ColorMap_DBG5_BKWH::THE_INSTANCE.GetInterpolatedColor(10.0001,0,10,0xffff0000)==0xffff0000);
+    assert(ColorMap_DBG6_BKWH::THE_INSTANCE.GetInterpolatedColor(-10.0001,-10,1,0xffff0000)==0xffff0000);
+
+    // Check red-blue ramp
+    for (u32 pct = 0; pct <= 100; ++pct) {
+      float frac = 1.0*pct/100;
+      u32 rise = 255*frac;
+      u32 fall = 255*(1-frac);
+      u32 redToBlue = 0xff000000|(fall<<16)|(rise<<0);
+      assertColorsClose(ColorMap_DBG5_RDBU::THE_INSTANCE.GetInterpolatedColor(pct,0,100,0xffff0000),redToBlue);
+      assertColorsClose(ColorMap_DBG6_RDBU::THE_INSTANCE.GetInterpolatedColor(pct,0,100,0xffff0000),redToBlue);
+    }
+  }
+
+} /* namespace MFM */
+
