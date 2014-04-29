cf8660e2b0b363628d94adf989c9a72d3cac9f87
Dave Ackley <ackley@ackleyshack.com>
2014-04-29 04:16:28 -0600

    Somewhat general drawable panel (Panel commit part 6)

--- src/gui/include/Panel.h
+++ src/gui/include/Panel.h
@@ -3,7 +3,9 @@
 
 #include "itype.h"
 #include "Point.h"
-#include "SDL/SDL.h"
+#include "Rect.h"
+#include "Drawing.h"
+#include <stdio.h>      /* For FILE */
 
 namespace MFM {
 
@@ -11,36 +13,142 @@ namespace MFM {
   {
   protected:
 
-    u32 m_width, m_height;
+    const char * m_name;
 
-    Point<s32> m_renderPt;
+    Rect m_rect;
+    u32 m_bgColor;  // Default background color of this panel
+    u32 m_fgColor;  // Default foreground color of this panel
 
-    SDL_Surface* m_dest;
+    // My parent Panel, if any
+    Panel * m_parent;
+
+    // The top of the stack of panels I am parent to.  This is
+    // null when I have no kids
+    Panel * m_top;
+
+    // My position in my parent's doubly-linked circle of panels.
+    // These are null when I have no parent.
+    Panel * m_forward;    // Pointer to next guy above me in rendering order
+    Panel * m_backward;   // Pointer to next guy below me in rendering order
+
+    static void Indent(FILE * file, u32 count) ;
 
   public:
 
-    Panel();
+    Panel(u32 width=0, u32 height=0);
 
-    Panel(u32 width, u32 height);
+    virtual ~Panel();
 
-    Panel(SDL_Surface* dest);
+    void Insert(Panel * child, Panel * afterOrNull) ;
 
-    Panel(SDL_Surface* dest, u32 width, u32 height);
+    Panel * Pop() ;
 
-    void SetDestination(SDL_Surface* dest);
+    void Remove(Panel * child) ;
 
-    u32 GetWidth() {return m_width;}
+    u32 GetWidth() {return m_rect.GetWidth();}
 
-    u32 GetHeight() {return m_height;}
-
-    SDL_Surface* GetDestination() {return m_dest;}
+    u32 GetHeight() {return m_rect.GetHeight();}
 
     void SetDimensions(u32 width, u32 height);
 
-    void SetRenderPoint(Point<s32>& renderPt);
+    const UPoint & GetDimensions() const ;
 
-    void Render(void* rendObj);
+    void SetRenderPoint(const SPoint & renderPt);
 
+    const char * GetName() const { return m_name; }
+
+    void SetName(const char * name) { m_name = name; }
+
+    void Print(FILE * file, u32 indent = 0) const;
+
+    /**
+       Get the current background color.
+     */
+    u32 GetBackground() const {
+      return m_bgColor;
+    }
+
+    /**
+       Set the background color.  Returns prior value
+     */
+    u32 SetBackground(const u32 color) {
+      u32 old = m_bgColor;
+      m_bgColor = color;
+      return old;
+    }
+
+    /**
+       Get the current foreground color.
+     */
+    u32 GetForeground() const {
+      return m_fgColor;
+    }
+
+    /**
+       Set the foreground color.  Returns prior value
+     */
+    u32 SetForeground(const u32 color) {
+      u32 old = m_fgColor;
+      m_fgColor = color;
+      return old;
+    }
+
+
+    virtual void Paint(Drawing & config);
+
+    /**
+       Paint the component itself, excluding its border and any
+       children it may have.  The default implementation (which may be
+       accessed, if overridden, via this->Panel::PaintComponent) sets
+       the drawing foreground and background colors to the panel
+       colors, and Clear()s the Panel.
+     */
+    virtual void PaintComponent(Drawing & config);
+
+    /**
+       Paint the border of the Panel, if it has one.  The default
+       implementation paints a one pixel foreground color border.
+     */
+    virtual void PaintBorder(Drawing & config);
+
+    /**
+       Paint the subpanels of the Panel, if it has any.  In general
+       this should not be overridden.  The default implementation
+       draws each child in turn from the bottom of the stacking order
+       to the top.
+     */
+    virtual void PaintChildren(Drawing & config);
+
+    /**
+       Dispatch a mouse event to the appropriate subpanel depending on
+       the stacking order and the position of the mouse.  Panels should
+       override the Handle(SDL_MouseButtonEvent) and/or
+       Handle(SDL_MouseMotionEvent) method if they wish to handle such
+       events.  Returns true if any panel claimed to handle the event.
+     */
+    virtual bool Dispatch(SDL_Event & event, const Rect & rect);
+
+    /**
+       Respond to a MouseButtonEvent (this includes scroll wheel
+       events).  Return true if the event should be considered
+       handled.  The default implementation does nothing and returns
+       false.
+     */
+    virtual bool Handle(SDL_MouseButtonEvent & event)
+    {
+      //printf("Mouse %d@(%d,%d)", event.type, event.x, event.y);
+      return false;
+    }
+
+    /**
+       Respond to a MouseMotionEvent.  Return true if the event should
+       be considered handled.  The default implementation does nothing
+       and returns false.
+     */
+    virtual bool Handle(SDL_MouseMotionEvent & event)
+    {
+      return false;
+    }
   };
 } /* namespace MFM */
 #endif /*PANEL_H*/
--- src/gui/src/Panel.cpp
+++ src/gui/src/Panel.cpp
@@ -3,55 +3,220 @@
 
 namespace MFM {
 
-  Panel::Panel()
-  {
-    m_dest = NULL;
-    m_renderPt = Point<s32>(0, 0);
-  }
-
   Panel::Panel(u32 width, u32 height)
   {
-    m_width = width;
-    m_height = height;
-    m_renderPt = Point<s32>(0, 0);
+    SetDimensions(width, height);
+
+    m_forward = m_backward = 0;
+    m_parent = m_top = 0;
+
+    m_name = 0;
+
+    m_bgColor = Drawing::BLACK;
+    m_fgColor = Drawing::YELLOW;
   }
 
-  Panel::Panel(SDL_Surface* dest)
+  Panel::~Panel()
   {
-    m_dest = dest;
-    m_renderPt = Point<s32>(0, 0);
+
+    // Eject any content in us
+    while (m_top)
+      Remove(m_top);
+
+    // Eject us from our parent
+    if (m_parent)
+      m_parent->Remove(this);
   }
 
-  Panel::Panel(SDL_Surface* dest, u32 width, u32 height)
+  void Panel::Indent(FILE * file, u32 count)
   {
-    m_dest = dest;
-    m_width = width;
-    m_height = height;
-    m_renderPt = Point<s32>(0, 0);
+    for (u32 i = 0; i < count; ++i)
+      fprintf(file," ");
+  }
+  void Panel::Print(FILE * file, u32 indent) const
+  {
+    Indent(file,indent);
+    fprintf(file,"[");
+    if (GetName()) fprintf(file,"%s:", GetName());
+    fprintf(file,"%p",(void*) this);
+    fprintf(file,"(%d,%d)%dx%d,bg:%08x,fg:%08x",
+            m_rect.GetX(),
+            m_rect.GetY(),
+            m_rect.GetWidth(),
+            m_rect.GetHeight(),
+            m_bgColor,
+            m_fgColor);
+    if (m_top) {
+      Panel * p = m_top;
+      fprintf(file,"\n");
+      do {
+        p = p->m_forward;
+        p->Print(file, indent+2);
+      } while (p != m_top);
+      Indent(file,indent);
+    }
+    fprintf(file,"]\n");
   }
 
-  void Panel::SetDestination(SDL_Surface* dest)
+  void Panel::Insert(Panel * child, Panel * after)
   {
-    m_dest = dest;
+    if (!child) FAIL(NULL_POINTER);
+    if (child->m_parent) FAIL(ILLEGAL_ARGUMENT);
+
+    if (!m_top) {
+
+      if (after) FAIL(ILLEGAL_ARGUMENT);
+      m_top = child->m_forward = child->m_backward = child;
+
+    } else {
+
+      if (!after) after = m_top;
+      else if (after->m_parent != this) FAIL(ILLEGAL_ARGUMENT);
+
+      child->m_forward = after->m_forward;
+      child->m_backward = after;
+      after->m_forward->m_backward = child;
+      after->m_forward = child;
+    }
+
+    child->m_parent = this;
+  }
+
+  void Panel::Remove(Panel * child)
+  {
+    if (!child) FAIL(NULL_POINTER);
+    if (child->m_parent != this) FAIL(ILLEGAL_ARGUMENT);
+
+    if (child->m_forward == child)  // Single elt list
+      m_top = 0;
+    else {
+      if (m_top == child)
+        m_top = child->m_forward;
+      child->m_forward->m_backward = child->m_backward;
+      child->m_backward->m_forward = child->m_forward;
+    }
+    child->m_parent = 0;
+    child->m_forward = 0;
+    child->m_backward = 0;
   }
 
   void Panel::SetDimensions(u32 width, u32 height)
   {
-    m_width = width;
-    m_height = height;
+    m_rect.SetWidth(width);
+    m_rect.SetHeight(height);
   }
 
-  void Panel::SetRenderPoint(Point<s32>& renderPt)
+  const UPoint & Panel::GetDimensions() const
   {
-    m_renderPt = renderPt;
+    return m_rect.GetSize();
   }
 
-  void Panel::Render(void* rendObj)
+  void Panel::SetRenderPoint(const SPoint & renderPt)
   {
-    Drawing::DrawHLine(m_dest, 0, 0, m_width, 0xff000000);
-    Drawing::DrawVLine(m_dest, 0, 0, m_height, 0xff000000);
-    Drawing::DrawHLine(m_dest, m_height, 0, m_width, 0xff000000);
-    Drawing::DrawVLine(m_dest, m_width, 0, m_height, 0xff000000);
+    m_rect.SetPosition(renderPt);
   }
+
+  void Panel::Paint(Drawing & drawing)
+  {
+    Rect old, cur;
+    drawing.GetWindow(old);
+    drawing.TransformWindow(m_rect);
+    drawing.GetWindow(cur);
+
+    PaintComponent(drawing);
+    PaintBorder(drawing);
+
+    drawing.SetWindow(cur);
+    PaintChildren(drawing);
+
+    drawing.SetWindow(old);
+  }
+
+  void Panel::PaintChildren(Drawing & drawing)
+  {
+    if (m_top) {
+      Rect cur;
+      drawing.GetWindow(cur);
+
+      Panel * p = m_top;
+      do {
+        p = p->m_forward;
+        drawing.SetWindow(cur);
+        p->Paint(drawing);
+      } while (p != m_top);
+    }
+  }
+
+  void Panel::PaintComponent(Drawing & drawing)
+  {
+    drawing.SetForeground(m_fgColor);
+    drawing.SetBackground(m_bgColor);
+    drawing.Clear();
+  }
+
+  void Panel::PaintBorder(Drawing & drawing)
+  {
+    drawing.SetForeground(m_fgColor);
+    drawing.DrawRectangle(Rect(SPoint(),m_rect.GetSize()));
+  }
+
+  bool Panel::Dispatch(SDL_Event & event, const Rect & existing)
+  {
+    SPoint at;
+    SDL_MouseButtonEvent * button = 0;
+    SDL_MouseMotionEvent * motion = 0;
+
+    switch (event.type) {
+    case SDL_MOUSEMOTION:
+      motion = &event.motion;
+      at.SetX(motion->x);
+      at.SetY(motion->y);
+      break;
+    case SDL_MOUSEBUTTONUP:
+    case SDL_MOUSEBUTTONDOWN:
+      button = &event.button;
+      at.SetX(button->x);
+      at.SetY(button->y);
+      break;
+    default:
+      return false;
+    }
+
+    Rect newRect;
+    Drawing::TransformWindow(existing, m_rect, newRect);
+
+    if (!newRect.Contains(at))
+      return false;
+
+    if (m_top) {
+
+      // Scan kids from top down so more visible gets first crack
+      Panel * p = m_top;
+      do {
+        if (p->Dispatch(event, newRect))
+          return true;
+        p = p->m_backward;
+      } while (p != m_top);
+    }
+
+    // Here the hit is in us and none of our descendants wanted it.
+    // So it's ours if we do.
+
+    switch (event.type) {
+    case SDL_MOUSEMOTION:
+      return Handle(*motion);
+    case SDL_MOUSEBUTTONUP:
+      printf("Panel Button Up");
+      Print(stdout);
+      // FALL THROUGH
+    case SDL_MOUSEBUTTONDOWN:
+      return Handle(*button);
+    default:
+      break;
+    }
+
+    return false;
+  }
+
 } /* namespace MFM */
 
