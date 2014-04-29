5719d128b065efbf2599c8178f0ea59f395d9316
Dave Ackley <ackley@ackleyshack.com>
2014-04-29 04:08:00 -0600

    A multilocation resource locator.  And a font cache that uses it. (Panel commit part 3)

--- src/sim/include/Utils.h
+++ src/sim/include/Utils.h
@@ -6,9 +6,23 @@
 
 namespace MFM
 {
-  u64 GetDateTimeNow() ;
+  namespace Utils {
+    u64 GetDateTimeNow() ;
 
-  u64 GetDateTime(time_t t) ;
+    u64 GetDateTime(time_t t) ;
+
+    /**
+       Look in standard places for relativePath.  Return true if a
+       readable file was found, and fills result with an absolute path
+       (up to the given length) that was openable for reading at the
+       time it was checked.  If such a readable file is not found
+       anywhere, set result to a null string and return false.  Fails
+       with ILLEGAL_ARGUMENT if length is zero; will silently truncate
+       paths (and fail to find a file, or worse, possibly find the wrong
+       file) if length is too short.
+    */
+    bool GetReadableResourceFile(const char * relativePath, char * result, u32 length) ;
+  }
 }
 
 #endif /* UTILS_H */
--- src/sim/src/Utils.cpp
+++ src/sim/src/Utils.cpp
@@ -4,23 +4,48 @@
 #include <stdlib.h>
 
 namespace MFM {
-  u64 GetDateTimeNow() {
-    time_t t;
-    t = time(NULL);
-    return GetDateTime(t);
-  }
+  namespace Utils {
+    u64 GetDateTimeNow() {
+      time_t t;
+      t = time(NULL);
+      return GetDateTime(t);
+    }
 
-  u64 GetDateTime(time_t t) {
-    struct tm *tmp;
-    tmp = localtime(&t);
-    if (tmp == NULL) 
-      FAIL(ILLEGAL_STATE);
-    return 
-      (((((((u64) tmp->tm_year+1900)
-           *100+tmp->tm_mon+1)
-          *100+tmp->tm_mday)
-         *100+tmp->tm_hour)
-        *100+tmp->tm_min)
-       *100+tmp->tm_sec);
+    u64 GetDateTime(time_t t) {
+      struct tm *tmp;
+      tmp = localtime(&t);
+      if (tmp == NULL)
+        FAIL(ILLEGAL_STATE);
+      return
+        (((((((u64) tmp->tm_year+1900)
+             *100+tmp->tm_mon+1)
+            *100+tmp->tm_mday)
+           *100+tmp->tm_hour)
+          *100+tmp->tm_min)
+         *100+tmp->tm_sec);
+    }
+
+    bool GetReadableResourceFile(const char * relativePath, char * result, u32 length) {
+      if (!length) FAIL(ILLEGAL_ARGUMENT);
+
+      const char * (paths[]) = { ".", "~/.mfm", DSHARED_DIR };
+      const char * home = getenv("HOME");
+
+      for (u32 i = 0; i < sizeof(paths)/sizeof(paths[0]); ++i) {
+
+        const char * dir = paths[i];
+        if (dir[0] == '~' && home)
+          snprintf(result, length, "%s%s/%s",home,dir+1,relativePath);
+        else
+          snprintf(result, length, "%s/%s",dir,relativePath);
+        FILE * f = fopen(result,"r");
+        if (f) {
+          fclose(f);
+          return true;
+        }
+      }
+      result[0] = '\0';
+      return false;
+    }
   }
 }
