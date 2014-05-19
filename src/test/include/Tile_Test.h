8a23bd7a9e909570a4dad5d548f55b4d06dd7fb1
Dave Ackley <ackley@ackleyshack.com>
2014-05-19 03:01:15 -0600

    Redoing.. renaming in src/test

---
+++ src/test/include/Tile_Test.h
@@ -0,0 +1,17 @@
+#ifndef TILE_TEST_H      /* -*- C++ -*- */
+#define TILE_TEST_H
+
+#include "test_common.h"
+
+namespace MFM {
+
+class Tile_Test
+{
+public:
+
+  static void Test_tilePlaceAtom();
+};
+} /* namespace MFM */
+
+#endif /*TILE_TEST_H*/
+
