76c2c3cbb30a3ef385df31596eaad80b86f88681
Dave Ackley <ackley@ackleyshack.com>
2014-06-18 04:31:23 -0400

    New driver mfmc (making mfms and mfmcl).  Move dual driver to sim/

---
+++ src/drivers/mfmc/include/main.h
@@ -0,0 +1,12 @@
+/* -*- C++ -*- */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "itype.h"
+#include "AbstractDualDriver.h"
+#include "P3Atom.h"
+#include "ParamConfig.h"
+#include "Element_Dreg.h"
+#include "Element_Res.h"
+
+#endif /* MAIN_H */
---
+++ src/drivers/mfmc/src/main.cpp
@@ -0,0 +1,60 @@
+#include "main.h"
+
+namespace MFM
+{
+  typedef ParamConfig<64,4,8,40> OurParamConfig;
+  typedef P3Atom<OurParamConfig> OurAtom;
+  typedef CoreConfig<OurAtom, OurParamConfig> OurCoreConfig;
+  typedef GridConfig<OurCoreConfig, 5, 3> OurGridConfig;
+
+  struct MFMCDriver : public AbstractDualDriver<OurGridConfig>
+  {
+  private: typedef AbstractDualDriver<OurGridConfig> Super;
+
+  public:
+    virtual void AddDriverArguments()
+    {
+      Super::AddDriverArguments();
+    }
+
+    virtual void OnceOnly(VArguments& args)
+    {
+      Super::OnceOnly(args);
+    }
+
+    virtual void ReinitPhysics()
+    {
+      OurGrid& m_grid = GetGrid();
+
+      m_grid.Needed(Element_Dreg<OurCoreConfig>::THE_INSTANCE);
+      m_grid.Needed(Element_Res<OurCoreConfig>::THE_INSTANCE);
+      m_grid.Needed(Element_Wall<OurCoreConfig>::THE_INSTANCE);
+    }
+
+    virtual void ReinitEden()
+    {
+      OurGrid& m_grid = GetGrid();
+
+      OurAtom dreg(Element_Dreg<OurCoreConfig>::THE_INSTANCE.GetDefaultAtom());
+
+      SPoint dregPt(5, 5);
+
+      m_grid.PlaceAtom(dreg, dregPt);
+    }
+  };
+}
+
+int main(int argc, const char** argv)
+{
+  MFM::LOG.SetByteSink(MFM::STDERR);
+  MFM::LOG.SetLevel(MFM::LOG.ALL);
+
+  MFM::MFMCDriver sim;
+  sim.Init(argc, argv);
+
+  sim.Reinit();
+
+  sim.Run();
+
+  return 0;
+}
