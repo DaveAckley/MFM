a867c2dad2057ff3f83c737b8926a9feab0e3d6a
Dave Ackley <ackley@ackleyshack.com>
2014-06-18 04:49:56 -0400

    Use ElementRegistry in ExternalConfig_Test

--- src/test/src/ExternalConfig_Test.cpp
+++ src/test/src/ExternalConfig_Test.cpp
@@ -5,23 +5,33 @@
 #include "ExternalConfig_Test.h"
 #include "ZStringByteSource.h"
 #include "FileByteSink.h"  /* For STDERR */
+#include "Element_Dreg.h"
 
 namespace MFM
 {
 
-  void ExternalConfig_Test::Test_RunTests()
+  static void TestBasic()
   {
+    ElementRegistry<TestCoreConfig> ereg;
+    ereg.RegisterElement(Element_Dreg<TestCoreConfig>::THE_INSTANCE);
+
     Grid<TestGridConfig> grid;
-    ExternalConfig<TestGridConfig> cfg(grid);
+    ExternalConfig<TestGridConfig> cfg(grid, ereg);
     RegisterExternalConfigFunctions<TestGridConfig>(cfg);
 
-    ZStringByteSource zbs("RegisterElement(4Dreg118148428408201406116113117, d)\n"
+    ZStringByteSource zbs("RegisterElement(Dreg-118148428408201406116113117, d)\n"
                           "GA(d,30,31)\n"
                           "GA(d,31,32)\n"
                           "SetParameter(DregResRatio,10)\n");
     cfg.SetByteSource(zbs, "./configurations/ExternalConfig_Test.input");
     cfg.SetErrorByteSink(STDERR);
     cfg.Read();
+
+  }
+
+  void ExternalConfig_Test::Test_RunTests()
+  {
+    TestBasic();
   }
 
 }
