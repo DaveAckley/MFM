b3d315ce4778f78c564bb3b34e5a2f2e9eafe4af
Sixstring982 <Sixstring982@gmail.com>
2014-07-07 11:48:49 -0600

    South anchor for toolbox panel

--- src/gui/include/AbstractGUIDriver.h
+++ src/gui/include/AbstractGUIDriver.h
@@ -418,6 +418,7 @@ namespace MFM {
       m_gridPanel.SetBorder(Drawing::BLACK);
       m_gridPanel.SetGridRenderer(&m_grend);
       m_gridPanel.SetToolboxPanel(&m_toolboxPanel);
+      m_toolboxPanel.SetAnchor(ANCHOR_SOUTH);
       m_gridPanel.SetGrid(&Super::GetGrid());
 
       m_statisticsPanel.SetStatsRenderer(&m_srend);
