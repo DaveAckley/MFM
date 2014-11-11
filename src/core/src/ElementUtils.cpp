0359fa3c2e90c91b171b97455c14c437445c0bfe
Dave Ackley <ackley@ackleyshack.com>
2014-11-11 04:36:14 -0700

    Trent's ElementUtils (not sure why they're new in this branch)

---
+++ src/core/src/ElementUtils.cpp
@@ -0,0 +1,12 @@
+#include "ElementUtils.h"
+
+namespace MFM
+{
+  namespace ElementUtils
+  {
+    const SPoint VNNeighbors[4] =
+    {
+      SPoint(-1, 0), SPoint(1, 0), SPoint(0, -1), SPoint(0, 1)
+    };
+  }
+}
