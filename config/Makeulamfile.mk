7b437b25ee8305a3b5eafb09003ae54fcab5db1a
Dave Ackley <ackley@ackleyshack.com>
2014-12-10 10:06:01 -0700

    Improved ulam build support

--- config/Makeulamfile.mk
+++ config/Makeulamfile.mk
@@ -10,11 +10,15 @@
 #   USE_PATH       : Search path to set
 #
 # Targets:
-#   ulam_cppcompile
+#   ulam_cppcompile (ULAM_CPP_FILE is compiled ulam element)
+#   ulam_program    (ULAM_CPP_FILE is NAMEOFPROGRAM.cpp)
 
 COMPONENTNAME:=ulam
 MFM_TARGET:=linux
-COMMANDS:=1
+#COMMANDS:=1
+DEBUG:=1
+
+dodo:=$(shell printenv)
 ifdef MFM_ROOT_DIR
  export PATH=$(USE_PATH)
  BASEDIR:=$(MFM_ROOT_DIR)
@@ -23,23 +27,34 @@ endif
 
 INCLUDES += -I $(BASEDIR)/src/core/include -I $(ULAM_SRC_DIR)/../include
 
-#CPPFLAGS += -fdiagnostics-color=never
+#DEBUGS += -g2
 
-PHONY_TARGETS:=ulam_cppcompile ulam_checkvar
+PHONY_TARGETS:=ulam_program ulam_cppcompile ulam_checkvar
 
 ULAM_OBJ_FILE := $(patsubst %.cpp,$(ULAM_BLD_DIR)/%.o,$(ULAM_CPP_FILE))
+ULAM_EXE_FILE := $(patsubst %.cpp,$(ULAM_BIN_DIR)/%,$(ULAM_CPP_FILE))
 #$(info "UCF($(ULAM_CPP_FILE))")
 #$(info "UOF($(ULAM_OBJ_FILE))")
 #$(info "USD($(ULAM_SRC_DIR))")
 #$(info "MRD($(MFM_ROOT_DIR))")
 
-ulam_cppcompile:	ulam_checkvar $(ULAM_OBJ_FILE)
-	@if [ ! -r "$(ULAM_SRC_DIR)/$(ULAM_CPP_FILE)" ] ; then echo "Cannot read $(ULAM_SRC_DIR)/$(ULAM_CPP_FILE)"; exit 10 ; fi
+ulam_program:	ulam_cppcompile $(ULAM_EXE_FILE)
+	@if [ ! -r "$(ULAM_SRC_DIR)/$(ULAM_CPP_FILE)" ] ; then \
+	  echo "Cannot read $(ULAM_SRC_DIR)/$(ULAM_CPP_FILE)"; exit 10 ; fi
 	echo "COMPILED " $(ULAM_SRC_DIR)/$(ULAM_CPP_FILE)
 
-# Pattern rule for cpp files
+ulam_cppcompile:	ulam_checkvar $(ULAM_OBJ_FILE)
+	@if [ ! -r "$(ULAM_SRC_DIR)/$(ULAM_CPP_FILE)" ] ; then \
+	  echo "Cannot read $(ULAM_SRC_DIR)/$(ULAM_CPP_FILE)"; exit 10 ; fi
+	echo "COMPILED " $(ULAM_SRC_DIR)/$(ULAM_CPP_FILE)
+
+# Pattern rule for cpp files (matches $(ULAM_OBJ_FILE))
 $(ULAM_BLD_DIR)/%.o:	$(ULAM_SRC_DIR)/%.cpp $(ALLDEP) #$(info DJDHD $(ULAM_BLD_DIR)/%.d)
 	$(GPP) $(OPTS) $(DEBUGS) $(CPPFLAGS) $(DEFINES) -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+
+# Rule for linking ulam programs
+$(ULAM_EXE_FILE):	 $(ULAM_OBJ_FILE)
+	$(GPP) $(LDFLAGS) $(ULAM_OBJ_FILE) $(LIBS) -o $@
 
 ulam_checkvar:
 	@if [ ! -d "$(ULAM_SRC_DIR)" ] ; then echo "ULAM_SRC_DIR='$(ULAM_SRC_DIR)' is not a dir"; exit 11 ; fi
@@ -47,7 +62,8 @@ ulam_checkvar:
 	@if [ ! -d "$(ULAM_BIN_DIR)" ] ; then echo "ULAM_BIN_DIR='$(ULAM_BIN_DIR)' is not a dir"; exit 13 ; fi
 	@if [ ! -d "$(MFM_ROOT_DIR)" ] ; then echo "MFM_ROOT_DIR='$(MFM_ROOT_DIR)' is not a dir"; exit 14 ; fi
 
-LIBS += -L $(ULAM_DIR)
-LIBS += -l mfmulam
+LIBS := -L $(ULAM_BLD_DIR) -l culam $(LIBS)
+
+LIBS += -L $(BASEDIR)/build/core -l mfmcore
 
 .PHONY:	$(PHONY_TARGETS)
