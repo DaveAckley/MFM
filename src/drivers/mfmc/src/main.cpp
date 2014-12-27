5bb64502db4f9db75081135b837a8c9c52c6a30e
Dave Ackley <ackley@ackleyshack.com>
2014-12-26 18:38:23 -0700

    Add more grid configs because compilation was too fast

--- src/drivers/mfmc/src/main.cpp
+++ src/drivers/mfmc/src/main.cpp
@@ -15,6 +15,14 @@ namespace MFM
   typedef GridConfig<OurCoreConfigStd, 5, 3> OurGridConfigStd;
 
   /////
+  // Alternate model (flatter space)
+
+  typedef ParamConfig<96,4,8,68> OurParamConfigAlt;
+  typedef P3Atom<OurParamConfigAlt> OurAtomAlt;
+  typedef CoreConfig<OurAtomAlt, OurParamConfigAlt> OurCoreConfigAlt;
+  typedef GridConfig<OurCoreConfigAlt, 3, 2> OurGridConfigAlt;
+
+  /////
   // Tiny model
 
   typedef ParamConfig<96,4,8,32> OurParamConfigTiny;
@@ -29,6 +37,22 @@ namespace MFM
   typedef P3Atom<OurParamConfigBig> OurAtomBig;
   typedef CoreConfig<OurAtomBig, OurParamConfigBig> OurCoreConfigBig;
   typedef GridConfig<OurCoreConfigBig, 8, 5> OurGridConfigBig;
+
+  /////
+  // BigTile model
+
+  typedef ParamConfig<96,4,8,176> OurParamConfigBigTile;
+  typedef P3Atom<OurParamConfigBigTile> OurAtomBigTile;
+  typedef CoreConfig<OurAtomBigTile, OurParamConfigBigTile> OurCoreConfigBigTile;
+  typedef GridConfig<OurCoreConfigBigTile, 1, 1> OurGridConfigBigTile;
+
+  /////
+  // MediumTile model
+
+  typedef ParamConfig<96,4,8,96> OurParamConfigMediumTile;
+  typedef P3Atom<OurParamConfigMediumTile> OurAtomMediumTile;
+  typedef CoreConfig<OurAtomMediumTile, OurParamConfigMediumTile> OurCoreConfigMediumTile;
+  typedef GridConfig<OurCoreConfigMediumTile, 1, 1> OurGridConfigMediumTile;
 
   template <class GC>
   struct MFMCDriver : public AbstractDualDriver<GC>
@@ -118,9 +142,36 @@ namespace MFM
     return 0;
   }
 
+  static int RunAlternate(int argc, const char** argv)
+  {
+    MFMCDriver<OurGridConfigAlt> sim;
+    sim.ProcessArguments(argc, argv);
+    sim.Init();
+    sim.Run();
+    return 0;
+  }
+
   static int RunBig(int argc, const char** argv)
   {
     MFMCDriver<OurGridConfigBig> sim;
+    sim.ProcessArguments(argc, argv);
+    sim.Init();
+    sim.Run();
+    return 0;
+  }
+
+  static int RunBigTile(int argc, const char** argv)
+  {
+    MFMCDriver<OurGridConfigBigTile> sim;
+    sim.ProcessArguments(argc, argv);
+    sim.Init();
+    sim.Run();
+    return 0;
+  }
+
+  static int RunMediumTile(int argc, const char** argv)
+  {
+    MFMCDriver<OurGridConfigMediumTile> sim;
     sim.ProcessArguments(argc, argv);
     sim.Init();
     sim.Run();
@@ -164,6 +215,21 @@ int main(int argc, const char** argv)
   if (EndsWith(argv[0],"_l"))
   {
     return MFM::RunBig(argc, argv);
+  }
+
+  if (EndsWith(argv[0],"_1l"))
+  {
+    return MFM::RunBigTile(argc, argv);
+  }
+
+  if (EndsWith(argv[0],"_1m"))
+  {
+    return MFM::RunMediumTile(argc, argv);
+  }
+
+  if (EndsWith(argv[0],"_a"))
+  {
+    return MFM::RunAlternate(argc, argv);
   }
 
   return MFM::RunMedium(argc, argv);
