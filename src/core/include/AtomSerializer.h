e89c0445726b30f5748bce1fee8cea7933b723ad
Dave Ackley <ackley@ackleyshack.com>
2015-06-25 23:27:48 -0600

    AtomTypeFormatter

--- src/core/include/AtomSerializer.h
+++ src/core/include/AtomSerializer.h
@@ -33,9 +33,59 @@
 #include "Atom.h"
 #include "BitVector.h"
 #include "ByteSerializable.h"
+#include "LineCountingByteSource.h"
 
 namespace MFM
 {
+
+  /**
+   * An AtomTypeFormatter (somehow) maps from an atom to a text
+   * representation of that atom's type, and (later) from that text
+   * representation back to a default atom of the original atom's
+   * element if it exists.  The primary reason this exists is that the
+   * current simulator may be using a different element<->type number
+   * mapping compared to the simulator that saved the atom's type.
+   */
+  template <class AC>
+  class AtomTypeFormatter
+  {
+  public:
+    typedef AC ATOM_CONFIG;
+
+    // Extract short names for parameter types
+    typedef typename ATOM_CONFIG::ATOM_TYPE T;
+
+    AtomTypeFormatter() { }
+    virtual ~AtomTypeFormatter() { }
+
+    /**
+       Read an atom type name (in some unspecified format) off the
+       given LineCountingByteSource.  Return false (perhaps with an
+       error message issued) if no legal element name can be read, in
+       which case destAtom is unaltered.  Otherwise, set destAtom to a
+       default atom of the type identified by the read name, and
+       return true.  This operation must invert PrintAtomType() for
+       any given AtomTypeFormatter.
+
+       \sa PrintAtomType()
+     */
+    virtual bool ParseAtomType(LineCountingByteSource & bs, T & destAtom) = 0;
+
+    /**
+       Print the name (in some unspecified format) of the given atom
+       on the given ByteSink.  Note that atom might be insane or have
+       an illegal type, and if so, the AtomTypeFormatter must print
+       some representation that reliably causes its ParseAtomType() to
+       return false given that same representation.
+
+       This operation must be inverted by ParseAtomType() for any
+       given AtomTypeFormatter.
+
+       \sa ParseAtomType()
+     */
+    virtual void PrintAtomType(const T & atom, ByteSink & bs) = 0;
+  };
+
 
   /**
    * A wrapper class to use for making an Atom ByteSerializable.  We
