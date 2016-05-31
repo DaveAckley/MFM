874ca4c9835410e8c8aae3bd61199ecc0989ddf5
Dave Ackley <ackley@ackleyshack.com>
2016-05-31 03:47:22 -0600

    MFM_API_ASSERT_NULL and fail code NON_NULL_POINTER

--- src/core/include/Fail.h
+++ src/core/include/Fail.h
@@ -49,6 +49,7 @@ enum MFMFailureCodes{
 
 #define MFM_API_ASSERT(expr,code) do { if (__builtin_expect(!(expr), 0)) FAIL(code); } while (0)
 #define MFM_API_ASSERT_NONNULL(expr) MFM_API_ASSERT(expr,NULL_POINTER)
+#define MFM_API_ASSERT_NULL(expr) MFM_API_ASSERT((expr)==0,NON_NULL_POINTER)
 #define MFM_API_ASSERT_ZERO(expr) MFM_API_ASSERT((expr)==0,NON_ZERO)
 #define MFM_API_ASSERT_NONZERO(expr) MFM_API_ASSERT((expr)!=0,ZERO)
 #define MFM_API_ASSERT_ARG(expr) MFM_API_ASSERT(expr,ILLEGAL_ARGUMENT)
