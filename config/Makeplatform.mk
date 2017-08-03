8ddd59d3ecc3775e29dfbcad54eee842edcab29c
Dave Ackley <ackley@ackleyshack.com>
2017-08-03 08:28:13 -0600

    More uname -m values

--- config/Makeplatform.mk
+++ config/Makeplatform.mk
@@ -1,5 +1,5 @@
 # This needs to be reworked, but for now (heh), we're saying that
-# X86_64 and X86 is platform 'linux' and armv7l is platform 'tile'
+# the x86's are platform 'linux' and armv7l is platform 'tile'
 MACHINE_TYPE:=$(shell uname -m)
 
 PLATFORM:=
@@ -7,6 +7,15 @@ ifeq ($(MACHINE_TYPE), x86_64)
 PLATFORM:=linux
 endif
 ifeq ($(MACHINE_TYPE), x86)
+PLATFORM:=linux
+endif
+ifeq ($(MACHINE_TYPE), i386)
+PLATFORM:=linux
+endif
+ifeq ($(MACHINE_TYPE), i586)
+PLATFORM:=linux
+endif
+ifeq ($(MACHINE_TYPE), i686)
 PLATFORM:=linux
 endif
 ifeq ($(MACHINE_TYPE), armv7l)
