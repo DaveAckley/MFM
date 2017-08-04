bd0fa16d1f583ebfbcdbc8057a27a3bd3db2097c
Dave Ackley <ackley@ackleyshack.com>
2017-08-04 04:34:45 -0600

    Further obscure our returning a pointer to a local

--- src/platform-tile/src/IsLocalPlatformSpecific.cpp
+++ src/platform-tile/src/IsLocalPlatformSpecific.cpp
@@ -1,9 +1,16 @@
 #include "IsLocalPlatformSpecific.h"
 
+#include <stdint.h> /* for intptr_t, grrr */
+
 void * __attribute__ ((noinline)) getStackBound()
 {
+  /* gcc-5_5.4.0 sees through the 'void * ret = &local; return ret;'
+     code that was previously here.  Grrr.  So we're trying a
+     roundtrip through intptr_t, despite its problems.  (Discovered on
+     ubuntu xenial.)
+  */
   char local;
-  void * ret = &local;
+  intptr_t obscure = (intptr_t) &local;
   asm(""); // try to block compiler optimizations
-  return ret;
+  return (void *) obscure;
 }
