28817a4a1c681880bc228911d55b5169695b6899
Dave Ackley <ackley@ackleyshack.com>
2019-06-11 02:26:30 -0600

    New TextPanel methods
     - TextPanel::SetFontHeightAdjust modifies the interline skip relative to that specified by the font
     - TextPanel::SetElevatorWidth modiries the width of the scrolling elevator, so, in particular, it can be set 0

--- src/gui/include/TextPanel.h
+++ src/gui/include/TextPanel.h
@@ -54,12 +54,14 @@ namespace MFM {
       , m_dragStartElevatorBottom(0)
       , m_panelHeight(100)
       , m_fontHeight(10)
+      , m_fontHeightAdjust(0)
       , m_neededHeight(10)
       , m_linesInPanel(10)
       , m_textLines(1)
       , m_elevatorRange(1)
       , m_elevatorBottom(0)
       , m_elevatorHeight(10)
+      , m_elevatorWidth(ELEVATOR_WIDTH_DEFAULT)
     {
       SetName("TextPanel");
       SetRenderPoint(SPoint(0, 0));
@@ -74,6 +76,18 @@ namespace MFM {
 
     TextPanelByteSink & GetByteSink() {
       return m_text;
+    }
+
+    s32 SetFontHeightAdjust(s32 newadjust) {
+      s32 old = m_fontHeightAdjust;
+      m_fontHeightAdjust = newadjust;
+      return old;
+    }
+
+    u32 SetElevatorWidth(u32 newelevatorwidth) {
+      u32 old = m_elevatorWidth;
+      m_elevatorWidth = newelevatorwidth;
+      return old;
     }
 
   protected:
@@ -148,7 +162,7 @@ namespace MFM {
         switch (event.button) {
 
         case SDL_BUTTON_LEFT:
-          if (pt.GetX() < (s32) ELEVATOR_WIDTH) {
+          if (pt.GetX() < (s32) m_elevatorWidth) {
             m_leftButtonDragStart = SPoint(event.x,event.y);//pt;
             m_dragStartElevatorBottom = m_elevatorBottom;
           }
@@ -175,7 +189,7 @@ namespace MFM {
       pt.Set(event.x - pt.GetX(),
              event.y - pt.GetY());
 
-      if ((pt.GetX() < (s32) ELEVATOR_WIDTH) &&
+      if ((pt.GetX() < (s32) m_elevatorWidth) &&
           (mbe.m_buttonMask & (1 << SDL_BUTTON_LEFT)) !=0) {
         SPoint nowAt(event.x, event.y);
         s32 deltay = nowAt.GetY() - m_leftButtonDragStart.GetY();
@@ -187,7 +201,7 @@ namespace MFM {
     }
 
   private:
-    static const u32 ELEVATOR_WIDTH = 18;
+    static const u32 ELEVATOR_WIDTH_DEFAULT = 18;
     static const u32 ELEVATOR_COLOR = 0xff007f00;
 
     TextPanelByteSink m_text;
@@ -199,12 +213,14 @@ namespace MFM {
 
     u32 m_panelHeight;
     u32 m_fontHeight;
+    s32 m_fontHeightAdjust;
     u32 m_neededHeight;
     u32 m_linesInPanel;
     u32 m_textLines;
     u32 m_elevatorRange;
     u32 m_elevatorBottom;
     u32 m_elevatorHeight;
+    u32 m_elevatorWidth;
 
     // If we move elevatorBottom this many pixels, how many bottom lines
     // is that, based on the most-recent prior render?
@@ -227,7 +243,9 @@ namespace MFM {
       if (!font) return;  // WTF?
 
       m_fontHeight = TTF_FontLineSkip(font);
-      if (m_fontHeight == 0)
+      m_fontHeight += m_fontHeightAdjust;
+      
+      if (m_fontHeight <= 0)
         m_fontHeight = 1;   // WTF!
 
       m_textLines = m_text.GetLines();
@@ -256,7 +274,8 @@ namespace MFM {
 
       m_elevatorBottom = m_elevatorRange * (m_textLines - m_bottomLineShown) / m_textLines;
 
-      drawing.FillRect(1, m_elevatorBottom, ELEVATOR_WIDTH-2, m_elevatorHeight, ELEVATOR_COLOR);
+      if (m_elevatorWidth > 2)
+        drawing.FillRect(1, m_elevatorBottom, m_elevatorWidth-2, m_elevatorHeight, ELEVATOR_COLOR);
 
       s32 y = m_panelHeight;
       for (s32 line = m_textLines - m_bottomLineShown - 1; line >= 0; --line) {
@@ -266,7 +285,7 @@ namespace MFM {
           break;
         }
         drawing.BlitText(zline,
-                         SPoint(ELEVATOR_WIDTH, y),
+                         SPoint(m_elevatorWidth, y),
                          UPoint(GetWidth(), m_fontHeight));
         y -= m_fontHeight;
         if (y < 0) break;
