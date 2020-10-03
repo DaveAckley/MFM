08eec6ab93b923a16375271f7d221f8265d64b07
Dave Ackley <ackley@livingcomputation.org>
2020-10-03 14:33:22 -0600

    Flesh out T2FlashTrafficManager

--- src/drivers/mfmt2/include/T2FlashTrafficManager.h
+++ src/drivers/mfmt2/include/T2FlashTrafficManager.h
@@ -2,50 +2,153 @@
 #ifndef T2FLASHTRAFFICMANAGER_H
 #define T2FLASHTRAFFICMANAGER_H
 
-#include "TimeoutAble.h"
+#include <string>
+#include <map>
+#include <vector>
+#include <set>
+
+#include "T2Types.h"
+#include "TimeoutAble.h"  // for time_before
 #include "ITCIterator.h"
 #include "T2PacketBuffer.h"
+#include "FlashTraffic.h"
 #include "Panel.h"
 #include "dirdatamacro.h"  // For DIR6*
+#include "T2Utils.h"
 
 namespace MFM {
 
   struct T2FlashCommandLabel; // FORWARD
   
-#define ALL_T2FLASH_BUTTON_NAMES()      \
-  XX(boot,1)                            \
-  XX(off,1)                             \
-  XX(tile,1)                            \
-  XX(sites,1)                           \
-  XX(less,0)                            \
-  XX(down,0)                            \
-  XX(dist,0)                            \
-  XX(up,0)                              \
-  XX(more,0)                            \
-  XX(dismiss,0)                         \
-  XX(clear,0)                           \
-  XX(gogogo,0)                          \
+#define T2FLASH_CATEG(nam) T2FLASH_CATEG_##nam
+#define T2FLASH_CMD(categ,nam) T2FLASH_CMD_##categ##_##nam
+#define T2FLASH_CMD_VALUE(categ,num) ((T2FLASH_CATEG(categ)<<4)|num)
+#define T2FLASH_CMD_STRING(categ,nam) ("_" #categ "_" #nam)
 
+#define T2FLASH_CMD_NAME(categ,nam) (#categ ":" #nam)
+
+#define T2FLASH_CONTROL_CMD(nam) T2FLASH_CONTROL_CMD_##nam
+#define T2FLASH_CONTROL_CMD_STRING(nam) ("_fcdist_" #nam)
   
+
+#define ALL_FLASH_CMD_CATEGORIES()     \
+  XX(t2t,0)                            \
+  XX(mfm,1)                            \
+  XX(phy,2)                            \
+  XX(dsp,3)                            \
+  /*CATEGORIES 4..7 RESERVED*/         \
+  /* NOTE VALUES 8..15 ILLEGAL! */     \
+  
+
+#define ALL_FLASH_CMDS()               \
+  XX(t2t,0,boot)                       \
+  XX(t2t,1,off)                        \
+  XX(t2t,2,xcdm)                       \
+  /* t2t,3..15 reserved*/              \
+  XX(mfm,0,run)                        \
+  XX(mfm,1,pause)                      \
+  XX(mfm,2,crash)                      \
+  XX(mfm,3,quit)                       \
+  /* mfm,4..15 reserved*/              \
+  XX(phy,0,clear)                      \
+  XX(phy,1,seed1)                      \
+  XX(phy,2,seed2)                      \
+  /* phy,3..15 reserved*/              \
+  XX(dsp,0,sites)                      \
+  XX(dsp,1,tile)                       \
+  XX(dsp,2,cdm)                        \
+  XX(dsp,3,tq)                         \
+  XX(dsp,4,log)                        \
+  /* dsp,5..15 reserved*/              \
+
+#define ALL_FLASH_COMMANDABLE_BUTTON_PANELS() \
+  XX(t2t,boot,T2Info_Boot_Button)             \
+  XX(t2t,off,T2Info_Off_Button)               \
+  XX(t2t,xcdm,T2Info_KillCDM_Button)          \
+  XX(mfm,run,PhysicsCtl_Run)                  \
+  XX(mfm,pause,PhysicsCtl_Pause)              \
+  XX(mfm,crash,T2Info_Crash_Button)           \
+  XX(mfm,quit,T2Info_Crash_Button)            \
+  XX(phy,clear,PhysicsCtl_Button_Clear)       \
+  XX(phy,seed1,PhysicsCtl_Button_Seed1)       \
+  XX(phy,seed2,PhysicsCtl_Button_Seed2)       \
+  XX(dsp,sites,GlobalMenu_Button_Sites)       \
+  XX(dsp,tile,GlobalMenu_Button_T2Viz)        \
+  XX(dsp,cdm,GlobalMenu_Button_CDM)           \
+  XX(dsp,tq,GlobalMenu_Button_TQ)             \
+  XX(dsp,log,GlobalMenu_Button_Log)           \
+
+#define T2FLASH_DIST_PANEL(nam) ("Grid_k_fcdist_" #nam)
+
+  enum T2FlashCategory {
+#define XX(nam,num) T2FLASH_CATEG(nam) = num,
+     ALL_FLASH_CMD_CATEGORIES()
+#undef XX
+     T2FLASH_CATEG__COUNT
+  };
+
   enum T2FlashCmd {
-#define XX(N,OP) T2FLASH_CMD_##N,
-     ALL_T2FLASH_BUTTON_NAMES()
+#define XX(categ,num,nam) T2FLASH_CMD(categ,nam) = T2FLASH_CMD_VALUE(categ,num),
+     ALL_FLASH_CMDS()
+#undef XX
      T2FLASH_CMD__COUNT
   };
 #undef XX      
+
+  typedef std::map<std::string,u32> PanelSuffixToFlashCmd;
+  typedef std::map<u32,std::string> U32ToString;
+
+  struct TimedFlashTraffic {
+    TimedFlashTraffic(const FlashTraffic packet) ;
+    const FlashTraffic mPacket;
+    const u32 mWhen;
+    bool operator<(const TimedFlashTraffic & rhs) const {
+      return time_before(mWhen,rhs.mWhen);
+    }
+  };
+  typedef std::multiset<TimedFlashTraffic> MultisetTimedFlashTraffic;
+  typedef std::map<u16,FlashTraffic> OriginKeyToFlashTraffic;
+
+#define ALL_FLASH_TRAFFIC_CONTROL_PANELS() \
+  XX(less)                                 \
+  XX(down)                                 \
+  XX(dist)                                 \
+  XX(up)                                   \
+  XX(more)                                 \
+  XX(prepared)                             \
+  XX(gogogo)                               \
+
+
+  enum T2FlashControlCmd {
+#define XX(nam) T2FLASH_CONTROL_CMD(nam),
+    ALL_FLASH_TRAFFIC_CONTROL_PANELS()
+#undef XX
+    T2FLASH_CONTROL_CMD__COUNT
+  };
 
   struct T2FlashTrafficManager : public TimeoutAble {
     typedef TimeoutAble Super;
 
     virtual const char * getName() const { return "FlashMgr"; }
 
+    T2FlashControlCmd findControlCmd(const char * name) const;
+
     T2FlashCmd findCmd(const char * name) const ;
+    const char * getCmdName(T2FlashCmd cmd) const ;
 
     const char * getPanelName(T2FlashCmd cmd) const ;
 
     bool isOp(T2FlashCmd cmd) const ;
 
-    void doCmd(T2FlashCmd cmd) ;
+    bool matchesPacketInPQ(const FlashTraffic packet, bool & diffOrigin) const ;
+
+    bool matchesPreviousFromOrigin(const FlashTraffic packet) const ;
+
+    void acceptPacket(const FlashTraffic packet) ;
+
+
+    //    void doCmd(T2FlashCmd cmd) ;
+    void doControlCmd(T2FlashControlCmd cmd) ;
 
     void onClick(T2FlashCommandLabel * button) ;
 
@@ -60,33 +163,52 @@ namespace MFM {
     s32 setRange(s32 range) ;
     s32 getRange() const ;
 
-    void clearPending() ;
-    void setPending(T2FlashCmd cmd) ;
-    void executePending() ;
-    T2FlashCmd getPending() const ;
-    void showPending() const ;
+    void clearPrepared() ;
+    void setPrepared(T2FlashCmd cmd) ;
+    void launchPreparedCommand() ;
+    T2FlashCmd getPrepared() const ;
+    T2FlashCommandLabel & getPreparedPanel() const ;
+    void showPrepared() const ;
 
     void dismissPanel() ;
 
     T2FlashCommandLabel & getGOGOGO() const ;
 
-  private:
+    void report(const FlashTraffic pkt, ByteSink& to) ;
 
+  private:
+#if 0
+    PanelSuffixToFlashCmd m_suffixToCmd;
+#endif
+    U32ToString m_cmdToCmdName;
+    U32ToString m_cmdToButtonPanelName;
+
+    void initMaps() ;
     void showRange() const ;
 
     void processTraffic() ;
 
     void handleInboundTraffic() ;
 
-    void handleFlashPacket(T2PacketBuffer & pb) ;
+    void handleInboundPacket(T2PacketBuffer & pb) ;
 
-    void sendFlashPacket(T2FlashCmd cmd, u32 ttl) ;
+    void receiveFlashPacket(const FlashTraffic packet) ;
+
+    void sendFlashPacket(const FlashTraffic packet) ;
+
+    void setGoGoColors(u32 now) ;
+
+    void managePending(TimeQueue& srcTQ) ;
+
+    void executeFlashTrafficCommand(T2FlashCmd cmd) ;
 
     u32 mTTL;
     
     s32 mLastIndex;
 
-    T2FlashCmd mPendingCmd;
+    FlashTraffic mPreparedCmd;
+    OriginKeyToFlashTraffic mOriginKeyToFlashTraffic;
+    MultisetTimedFlashTraffic mMultisetTimedFlashTraffic;
 
     int open() ;
     int close() ;
--- src/drivers/mfmt2/src/T2FlashTrafficManager.cpp
+++ src/drivers/mfmt2/src/T2FlashTrafficManager.cpp
@@ -4,53 +4,108 @@
 #include "SDLI.h"
 #include "Logger.h"
 #include "FlashTraffic.h"
+#include "AbstractRadioButton.h"
 
 #include <string.h> /*For strcmp, strerror */
 
 namespace MFM {
+  static u32 getNow() {
+    T2Tile & tile = T2Tile::get();
+    return tile.getTQ().now();
+  }
 
-#define XX(N,OP) "Flash_Button_CMD_" #N,
-  static const char * t2FlashButtonNames[] = {
-     ALL_T2FLASH_BUTTON_NAMES()
-     0
-  };
-#undef XX
+  const u32 MS_DELAY_PER_TTL = 666u; // 3rd future tick @~250ms/tick
+  TimedFlashTraffic::TimedFlashTraffic(const FlashTraffic packet)
+    : mPacket(packet)
+    , mWhen(getNow() + MS_DELAY_PER_TTL * mPacket.mTimeToLive)
+  { }
 
+#if 0
 #define XX(N,OP) OP,
   static const bool t2FlashButtonIsOp[] = {
      ALL_T2FLASH_BUTTON_NAMES()
      0
   };
 #undef XX
+#endif
 
-  T2FlashCmd T2FlashTrafficManager::findCmd(const char * name) const {
-    MFM_API_ASSERT_NONNULL(name);
-    for (u32 i = 0; i < T2FLASH_CMD__COUNT; ++i) {
-      if (!strcmp(name,t2FlashButtonNames[i]))
-        return (T2FlashCmd) i;
-    }
+
+  void T2FlashTrafficManager::initMaps() {
+#if 0
+#define XX(categ,num,nam) m_suffixToCmd[ T2FLASH_CMD_STRING(categ,nam) ] = T2FLASH_CMD_VALUE(categ,num);
+    ALL_FLASH_CMDS()
+#undef XX
+#endif
+#define XX(categ,num,nam) m_cmdToCmdName[ T2FLASH_CMD(categ,nam) ] = T2FLASH_CMD_NAME(categ,nam);
+    ALL_FLASH_CMDS()
+#undef XX
+#define XX(categ,nam,panel) m_cmdToButtonPanelName[ T2FLASH_CMD(categ,nam) ] = #panel;
+      ALL_FLASH_COMMANDABLE_BUTTON_PANELS()
+#undef XX
+  }
+
+  const char * T2FlashTrafficManager::getCmdName(T2FlashCmd cmd) const {
+    U32ToString::const_iterator itr = m_cmdToCmdName.find(cmd);
+    if (itr == m_cmdToCmdName.end()) return "unkcmd?";
+    return itr->second.c_str();
+  }
+    
+  T2FlashCmd T2FlashTrafficManager::findCmd(const char * panelName) const {
+    MFM_API_ASSERT_NONNULL(panelName);
+    std::string spname(panelName);
+#define XX(categ,num,nam) if (endsWith(spname,T2FLASH_CMD_STRING(categ,nam))) return T2FLASH_CMD(categ,nam);
+    ALL_FLASH_CMDS();
+#undef XX    
     return T2FLASH_CMD__COUNT;
   }
 
+  T2FlashControlCmd T2FlashTrafficManager::findControlCmd(const char * panelName) const {
+    MFM_API_ASSERT_NONNULL(panelName);
+    std::string spname(panelName);
+#define XX(nam) if (endsWith(spname,T2FLASH_CONTROL_CMD_STRING(nam))) return T2FLASH_CONTROL_CMD(nam);
+    ALL_FLASH_TRAFFIC_CONTROL_PANELS();
+#undef XX    
+    return T2FLASH_CONTROL_CMD__COUNT;
+  }
+
+#if 0
   bool T2FlashTrafficManager::isOp(T2FlashCmd cmd) const {
     if (cmd < 0 || cmd >= T2FLASH_CMD__COUNT) return false;
     return t2FlashButtonIsOp[cmd];
   }
-
+#endif
+  
   const char * T2FlashTrafficManager::getPanelName(T2FlashCmd cmd) const {
+    FAIL(INCOMPLETE_CODE);
+#if 0
+    
     if (cmd < 0 || cmd >= T2FLASH_CMD__COUNT) return 0;
     return t2FlashButtonNames[cmd];
+#endif
+  }
+
+  void T2FlashTrafficManager::report(const FlashTraffic pkt, ByteSink& to) {
+    to.Printf("%s%c%s+%d/%d(%d,%d)",
+              Dirs::GetCode(pkt.mPktHdr&0x7),
+              pkt.checksumValid() ? '+' : '-',
+              getCmdName((T2FlashCmd) pkt.mCommand),
+              pkt.mTimeToLive,
+              pkt.mRange,
+              pkt.mOrigin.GetX(),
+              pkt.mOrigin.GetY()
+              );
   }
 
   T2FlashTrafficManager::T2FlashTrafficManager()
     : mTTL(8)
     , mLastIndex(-1)
-    , mPendingCmd(T2FLASH_CMD__COUNT)
+    , mPreparedCmd()
     , mFD(-1)
   {
   }
 
   void T2FlashTrafficManager::init() {
+    initMaps();
     T2Tile & tile = T2Tile::get();
     int ret = open();
     if (ret < 0) {
@@ -83,14 +138,16 @@ namespace MFM {
     return ret;
   }
 
-  void T2FlashTrafficManager::sendFlashPacket(T2FlashCmd cmd, u32 ttl) {
+  void T2FlashTrafficManager::sendFlashPacket(const FlashTraffic packet) {
     static ITCIteration dir6Iteration(T2Tile::get().getRandom(), 10);
 
     if (mFD < 0) FAIL(ILLEGAL_STATE);
     for (ITCIterator itr = dir6Iteration.begin(); itr.hasNext(); ) {
       u32 dir6 = itr.next();
-      FlashTraffic ft(0x80|dir6, cmd, mLastIndex, ttl);
-      ssize_t amt = write(mFD, (const char *) &ft, sizeof(ft));
+      Dir8 dir8 = mapDir6ToDir8(dir6);
+      if (!packet.canPropagateTo(dir8)) continue;
+      FlashTraffic tosend(packet, dir8);
+      ssize_t amt = write(mFD, (const char *) &tosend, sizeof(tosend));
       if (amt < 0) {
         if (errno == EHOSTUNREACH) continue;
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
@@ -119,17 +176,73 @@ namespace MFM {
         }
         FAIL(ILLEGAL_STATE);
       }
-      handleFlashPacket(pb);
+      handleInboundPacket(pb);
     }
   }
 
-  void T2FlashTrafficManager::handleFlashPacket(T2PacketBuffer & pb) {
+  void T2FlashTrafficManager::handleInboundPacket(T2PacketBuffer & pb) {
     const char * buf = pb.GetBuffer();
     u32 len = pb.GetLength();
-    if (len < 5)
-      LOG.Error("SHORT FLASH %d", len);
-    LOG.Message("FLASH: 0x%02x 0x%02x 0x%02x 0x%02x ",
-                buf[1], buf[2], buf[3], buf[4]);
+    if (!isStandardFlash(pb)) {
+      LOG.Error("NOT FLASH? 0x%02x+%d", buf[0], len);
+      return;
+    }
+    if (len < sizeof(FlashTraffic)) {
+      LOG.Error("SHORT FLASH? 0x%02x+%d", buf[0], len);
+      return;
+    }
+    FlashTraffic & ft = *(FlashTraffic*) buf;
+    receiveFlashPacket(ft);
+  }
+
+  bool T2FlashTrafficManager::matchesPacketInPQ(const FlashTraffic packet, bool & diffOrigin) const {
+    for (MultisetTimedFlashTraffic::const_iterator itr = mMultisetTimedFlashTraffic.begin();
+         itr != mMultisetTimedFlashTraffic.end(); ++itr) {
+      if (packet.matchesCIRO(itr->mPacket)) {
+        diffOrigin = false;
+        return true;
+      }
+      if (packet.matchesCIR(itr->mPacket)) {  // Might be loopback or other nonsense
+        diffOrigin = true;
+        return true;
+      }
+
+    }
+    return false;
+  }
+
+  bool T2FlashTrafficManager::matchesPreviousFromOrigin(const FlashTraffic packet) const {
+    BPoint origin = packet.mOrigin;
+    u16 key = (((u8) origin.GetX())<<8)|((u8) origin.GetY());
+    OriginKeyToFlashTraffic::const_iterator itr = mOriginKeyToFlashTraffic.find(key);
+    if (itr == mOriginKeyToFlashTraffic.end()) return false;
+    return packet.matchesCIRO(itr->second);
+  }
+
+  void T2FlashTrafficManager::acceptPacket(const FlashTraffic packet) {
+    mMultisetTimedFlashTraffic.insert(TimedFlashTraffic(packet));
+    BPoint origin = packet.mOrigin;
+    u16 key = (((u8) origin.GetX())<<8)|((u8) origin.GetY());
+    mOriginKeyToFlashTraffic[key] = packet;
+  }
+
+  void T2FlashTrafficManager::receiveFlashPacket(const FlashTraffic packet) {
+    OString32 buf;
+    report(packet,buf);
+    LOG.Message("rFP:%s",buf.GetZString());
+    if (!packet.checksumValid()) return;
+
+    bool diffOrigin = false; // True if holding same command from different origin
+    if (matchesPacketInPQ(packet,diffOrigin) && !diffOrigin) return; // exact dupe
+    if (matchesPreviousFromOrigin(packet)) return; // late dupe?
+
+    // If we don't already have this command, accept and remember, it
+    if (!diffOrigin) {
+      acceptPacket(packet);                  // accept
+      mPreparedCmd = packet;                 // remember
+    }
+    // And propagate anything that's not an exact dupe
+    sendFlashPacket(packet);               // propagate
   }
 
   bool T2FlashTrafficManager::flushPendingPackets() {
@@ -162,32 +275,77 @@ namespace MFM {
     return packetsHandled>0; /* nothing to read or flush finished */
   }
 
-
   T2FlashCommandLabel & T2FlashTrafficManager::getGOGOGO() const {
     T2Tile & tile = T2Tile::get();
     SDLI & sdli = tile.getSDLI();
-    const char * name = getPanelName(T2FLASH_CMD_gogogo);
-    MFM_API_ASSERT_NONNULL(name);
-    Panel * panel = sdli.lookForPanel(name);
+    Panel * panel = sdli.lookForPanel(T2FLASH_DIST_PANEL(gogogo));
     MFM_API_ASSERT_NONNULL(panel);
     T2FlashCommandLabel * label = dynamic_cast<T2FlashCommandLabel*>(panel);
     MFM_API_ASSERT_NONNULL(label);
     return *label;
   }
 
-  void T2FlashTrafficManager::onTimeout(TimeQueue& srcTQ) {
+  T2FlashCommandLabel & T2FlashTrafficManager::getPreparedPanel() const {
+    T2Tile & tile = T2Tile::get();
+    SDLI & sdli = tile.getSDLI();
+    Panel * panel = sdli.lookForPanel(T2FLASH_DIST_PANEL(prepared));
+    MFM_API_ASSERT_NONNULL(panel);
+    T2FlashCommandLabel * label = dynamic_cast<T2FlashCommandLabel*>(panel);
+    MFM_API_ASSERT_NONNULL(label);
+    return *label;
+  }
+
+  void T2FlashTrafficManager::setGoGoColors(u32 now) {
     T2FlashCommandLabel & gogo = getGOGOGO();
-    if (getPending() >= T2FLASH_CMD__COUNT) {
+    if (getPrepared() >= T2FLASH_CMD__COUNT) {
       gogo.SetEnabledBg(0x993333);
       gogo.SetEnabledFg(0x993333);
-    } else if ((srcTQ.now() >> 10)&1) {
+    } else if ((now >> 10)&1) {
       gogo.SetEnabledBg(0x00000000);
       gogo.SetEnabledFg(0xffffffff);
     } else {
       gogo.SetEnabledBg(0xffffffff);
       gogo.SetEnabledFg(0x00000000);
     }
+  }
+
+  void T2FlashTrafficManager::managePending(TimeQueue& srcTQ) {
+    while (1) {
+      MultisetTimedFlashTraffic::iterator oldest = mMultisetTimedFlashTraffic.begin();
+      if (oldest == mMultisetTimedFlashTraffic.end()) return; // Empty pq
+      const TimedFlashTraffic & elt = *oldest;
+      if (time_after_eq(elt.mWhen,srcTQ.now())) return;  // Execution time not yet reached
+      const FlashTraffic ready = elt.mPacket;                    // Copy command
+      mMultisetTimedFlashTraffic.erase(oldest);            // Delete oldest
+      executeFlashTrafficCommand((T2FlashCmd) ready.mCommand); // Do it
+    }
+  }
+  
+  void T2FlashTrafficManager::executeFlashTrafficCommand(T2FlashCmd cmd) {
+    U32ToString::iterator itr = m_cmdToButtonPanelName.find(cmd);
+    if (itr == m_cmdToButtonPanelName.end()) {
+      LOG.Message("XXX IMPLEMENT ME NOW NOW NOW: %s",getCmdName(cmd));
+    } else {
+      const char * buttonPanel = itr->second.c_str();
+      T2Tile & tile = T2Tile::get();
+      SDLI & sdli = tile.getSDLI();
+      Panel * panel = sdli.lookForPanel(buttonPanel);
+      if (!panel) LOG.Error("No '%s' panel found",buttonPanel);
+      else {
+        u32 mods = 0;
+        SDL_Event evt;
+        evt.type = SDL_MOUSEBUTTONUP;
+        MouseButtonEvent event(mods,evt);
+        panel->Handle(event);
+      }
+    }
+  }
+
+  void T2FlashTrafficManager::onTimeout(TimeQueue& srcTQ) {
+    setGoGoColors(srcTQ.now());
     processTraffic();
+    showPrepared();
+    managePending(srcTQ);
     schedule(srcTQ,250);
   }
 
@@ -199,42 +357,50 @@ namespace MFM {
     MFM_API_ASSERT_NONNULL(button);
     const char * name = button->GetName();
     MFM_API_ASSERT_NONNULL(name);
-    T2FlashCmd got = findCmd(name);
-    if (got >= T2FLASH_CMD__COUNT) {
+    T2FlashControlCmd got = findControlCmd(name);
+    if (got >= T2FLASH_CONTROL_CMD__COUNT) {
       LOG.Message("T2FlashTrafficManager::onClick?? %s", name);
       return;
     }
-    doCmd(got);
+    doControlCmd(got);
   }
 
-  void T2FlashTrafficManager::clearPending() {
-    setPending(T2FLASH_CMD__COUNT);
+  void T2FlashTrafficManager::clearPrepared() {
+    setPrepared(T2FLASH_CMD__COUNT);
   }
 
-  void T2FlashTrafficManager::setPending(T2FlashCmd cmd) {
-    mPendingCmd = cmd;
-    showPending();
+  void T2FlashTrafficManager::setPrepared(T2FlashCmd cmd) {
+    mPreparedCmd = cmd;
+    showPrepared();
   }
 
-  void T2FlashTrafficManager::executePending() {
-    T2FlashCmd cmd = getPending();
-    if (cmd < T2FLASH_CMD__COUNT) {
-      sendFlashPacket(cmd,getRange());
-      LOG.Message("EXECUTED FLASH TRAFFIC %d",cmd);
+  void T2FlashTrafficManager::launchPreparedCommand() {
+    T2FlashCmd cmd = getPrepared();
+    u32 range = getRange();
+    if (cmd < T2FLASH_CMD__COUNT && range > 0) {
+      FlashTraffic ft(0x80, cmd, ++mLastIndex, range);
+      sendFlashPacket(ft);      // ship it
+      acceptPacket(ft);         // also accept it
+      mPreparedCmd = ft;        // and remember it
+
+      LOG.Message("LAUNCHED FLASH TRAFFIC %d",cmd);
     }
     dismissPanel();
   }
 
-  T2FlashCmd T2FlashTrafficManager::getPending() const {
-    return mPendingCmd;
+  T2FlashCmd T2FlashTrafficManager::getPrepared() const {
+    T2Tile & tile = T2Tile::get();
+    SDLI & sdli = tile.getSDLI();
+    Panel & root = sdli.getRootPanel();
+    AbstractRadioButton * pushed = AbstractRadioButton::GetPushedIfAny("FC",&root);
+    if (!pushed) return T2FLASH_CMD__COUNT;
+    return findCmd(pushed->GetName());
   }
 
   void T2FlashTrafficManager::showRange() const {
     T2Tile & tile = T2Tile::get();
     SDLI & sdli = tile.getSDLI();
-    const char * name = getPanelName(T2FLASH_CMD_dist);
-    MFM_API_ASSERT_NONNULL(name);
-    Panel * panel = sdli.lookForPanel(name);
+    Panel * panel = sdli.lookForPanel(T2FLASH_DIST_PANEL(dist));
     MFM_API_ASSERT_NONNULL(panel);
     T2FlashCommandLabel * label = dynamic_cast<T2FlashCommandLabel*>(panel);
     if (label) {
@@ -242,30 +408,23 @@ namespace MFM {
       snprintf(buff,10,"%3d",getRange());
       label->SetText(buff);
     }
-    showPending();
+    showPrepared();
   }
 
 
-  void T2FlashTrafficManager::showPending() const {
-    T2Tile & tile = T2Tile::get();
-    SDLI & sdli = tile.getSDLI();
-    T2FlashCommandLabel & label = getGOGOGO();
-    T2FlashCmd cmd = getPending();
+  void T2FlashTrafficManager::showPrepared() const {
+    T2FlashCommandLabel & prepared = getPreparedPanel();
+    T2FlashCommandLabel & gogogo = getGOGOGO();
+    T2FlashCmd cmd = getPrepared();
     if (cmd >= T2FLASH_CMD__COUNT) {
-      label.SetText("");
-      label.SetEnabled(false);
+      prepared.SetText("--");
+      gogogo.SetEnabled(false);
     } else {
-      const char * cmdname = getPanelName(cmd);
-      MFM_API_ASSERT_NONNULL(cmdname);
-      Panel * cmdpanel = sdli.lookForPanel(cmdname);
-      MFM_API_ASSERT_NONNULL(cmdpanel);
-      T2FlashCommandLabel * cmdlabel = dynamic_cast<T2FlashCommandLabel*>(cmdpanel);
-      
       const u32 LEN = 100;
       char buff[LEN];
-      snprintf(buff,LEN,"%s%d",cmdlabel->GetText(),getRange());
-      label.SetText(buff);
-      label.SetEnabled(true);
+      snprintf(buff,LEN,"%s+%d", getCmdName(cmd), getRange());
+      prepared.SetText(buff);
+      gogogo.SetEnabled(true);
     }
   }
 
@@ -278,7 +437,7 @@ namespace MFM {
     else if (range > 255) range = 255;
     mTTL = (u32) range;
     showRange();
-    if (range == 0) clearPending();
+    if (range == 0) clearPrepared();
     return getRange();
   }
 
@@ -286,27 +445,29 @@ namespace MFM {
     T2Tile & tile = T2Tile::get();
     SDLI & sdli = tile.getSDLI();
     MenuManager & mm = sdli.getMenuManager();
-    clearPending();
+    clearPrepared();
     MenuAction ma;
     ma.mKeyToPress = SDLK_LEFT; /*RETURN*/
     mm.execute(ma, 0);
   }
 
-  void T2FlashTrafficManager::doCmd(T2FlashCmd cmd) {
+  void T2FlashTrafficManager::doControlCmd(T2FlashControlCmd cmd) {
     switch (cmd) {
-    case T2FLASH_CMD_less: setRange(getRange() / 2); break;
-    case T2FLASH_CMD_down: setRange(getRange() - 1); break;
-    case T2FLASH_CMD_dist: LOG.Message("DO IT"); break;
-    case T2FLASH_CMD_up:   setRange(getRange() + 1); break;
-    case T2FLASH_CMD_more: setRange(getRange() == 0 ? 1 : getRange() * 2); break;
-    case T2FLASH_CMD_dismiss: dismissPanel(); break;
-    case T2FLASH_CMD_clear: clearPending(); break;
+    case T2FLASH_CONTROL_CMD_less: setRange(getRange() / 2); break;
+    case T2FLASH_CONTROL_CMD_down: setRange(getRange() - 1); break;
+    case T2FLASH_CONTROL_CMD_dist: LOG.Message("DO IT"); break;
+    case T2FLASH_CONTROL_CMD_up:   setRange(getRange() + 1); break;
+    case T2FLASH_CONTROL_CMD_more: setRange(getRange() == 0 ? 1 : getRange() * 2); break;
+    case T2FLASH_CONTROL_CMD_gogogo: launchPreparedCommand(); break;
+    default: LOG.Error("WTF %d?",cmd); return;
+#if 0
+    case T2FLASH_CONTROL_CMD_dismiss: dismissPanel(); break;
+    case T2FLASH_CMD_clear: clearPrepared(); break;
     case T2FLASH_CMD_boot:
     case T2FLASH_CMD_off:
     case T2FLASH_CMD_tile:
-    case T2FLASH_CMD_sites: setPending(cmd); break;
-    case T2FLASH_CMD_gogogo: executePending(); break;
-    default: LOG.Error("WTF %d?",cmd); return;
+    case T2FLASH_CMD_sites: setPrepared(cmd); break;
+#endif
     }
   }
 }
